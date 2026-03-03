23078bac8d8bf5415d2f88856c1774c6ece8bb9f
mrty115 <72619381+marurunnnnn@users.noreply.github.com>
2026-03-03 10:16:49 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+﻿const CACHE_NAME = 'fcc-calc-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
+
+// インストール時にキャッシュ
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+// ネットワーク優先で取得（オフライン時はキャッシュ）
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
